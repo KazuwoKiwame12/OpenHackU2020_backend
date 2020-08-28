f5c668199ea8686966a5149366a006d42051320e
SoccerPlay32 <yahoosekainosekai@gmail.com>
2020-08-29 01:14:56 +0900

    finish issue7

--- Service/DateService/DateService.go
+++ Service/DateService/DateService.go
@@ -1,14 +1,12 @@
 package dateservice
 
 import (
-	"log"
 	"time"
 )
 
 //StartOfDay ...引数の開始日時を返す
 func StartOfDay(t time.Time) time.Time {
 	startOfDay := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.Local)
-	log.Print(startOfDay)
 	return startOfDay
 }
 
@@ -16,6 +14,5 @@ func StartOfDay(t time.Time) time.Time {
 func StartOfNextDay(t time.Time) time.Time {
 	startOfDay := StartOfDay(t)
 	startOfNextDay := startOfDay.AddDate(0, 0, 1)
-	log.Print(startOfNextDay)
 	return startOfNextDay
 }
