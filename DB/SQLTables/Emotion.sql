cb96af89276b5bad6c9e375f0d98eebc10d2979e
SoccerPlay32 <yahoosekainosekai@gmail.com>
2020-08-29 12:16:46 +0900

    Emotionモデル関連の修正と色の計算部分の修正と更新

--- DB/SQLTables/Emotion.sql
+++
@@ -1,6 +0,0 @@
-CREATE table emotions (
-    id SERIAL NOT NULL,
-    emotion integer NOT NULL,
-    point integer,
-    PRIMARY KEY (id)
-);
--- Service/PrefectureService/PrefectureService.go
+++ Service/PrefectureService/PrefectureService.go
@@ -1,11 +1,9 @@
 package prefectureservice
 
 import (
-	"image/color"
-	"strconv"
-
 	comment "github.com/KazuwoKiwame12/open-hack-u-2020-backend/DB/Model/Comment"
 	commentservice "github.com/KazuwoKiwame12/open-hack-u-2020-backend/Service/CommentService"
+	emotionservice "github.com/KazuwoKiwame12/open-hack-u-2020-backend/Service/EmotionService"
 )
 
 //PrefectureInfo ...クライアントに返すデータ
@@ -19,8 +17,8 @@ func GetPrefectureInfoList() []PrefectureInfo {
 	prefectureInfoList := []PrefectureInfo{}
 	for _, prefecture := range getAllPrefectures() {
 		commentList := comment.GetListByPrefecture(prefecture)
-		emotionList := commentservice.GetCommentEmotionList(commentList)
-		prefectureColor := calcColor(emotionList)
+		emotionIDList := commentservice.GetCommentEmotionIDList(commentList)
+		prefectureColor := emotionservice.AlfaBlend(emotionIDList)
 
 		prefectureInfo := PrefectureInfo{Prefecture: prefecture, Color: prefectureColor}
 		prefectureInfoList = append(prefectureInfoList, prefectureInfo)
@@ -82,50 +80,4 @@ func getAllPrefectures() [47]string {
 	}
 
 	return prefectureList
-}
-
-func calcColor(emotionList []int) string {
-	colorList := [4]color.NRGBA{}
-	colorList[0] = color.NRGBA{R: 255, G: 102, B: 255, A: 160} //喜
-	colorList[1] = color.NRGBA{R: 255, G: 51, B: 51, A: 160}   //怒
-	colorList[2] = color.NRGBA{R: 153, G: 153, B: 255, A: 160} //哀
-	colorList[3] = color.NRGBA{R: 204, G: 204, B: 51, A: 160}  //楽
-
-	mixColor := color.NRGBA{}
-
-	for index, emotion := range emotionList {
-		colorParam := convertEIDtoColorParam(emotion)
-		if index == 0 {
-			mixColor = colorList[colorParam]
-		} else {
-			alfaRate := (colorList[colorParam].A / 255)
-			newR := colorList[colorParam].R*alfaRate + mixColor.R*(1-alfaRate)
-			newG := colorList[colorParam].G*alfaRate + mixColor.G*(1-alfaRate)
-			newB := colorList[colorParam].B*alfaRate + mixColor.B*(1-alfaRate)
-			mixColor = color.NRGBA{R: newR, G: newG, B: newB, A: 205}
-		}
-	}
-
-	stringR := strconv.FormatInt(int64(mixColor.R), 16)
-	stringG := strconv.FormatInt(int64(mixColor.G), 16)
-	stringB := strconv.FormatInt(int64(mixColor.B), 16)
-
-	returnColor := stringR + stringG + stringB
-
-	return returnColor
-}
-
-func convertEIDtoColorParam(emotionID int) int {
-	result := 0
-	if 1 <= emotionID && emotionID <= 3 {
-		result = 0
-	} else if 4 <= emotionID && emotionID <= 6 {
-		result = 1
-	} else if 7 <= emotionID && emotionID <= 9 {
-		result = 2
-	} else if 10 <= emotionID && emotionID <= 12 {
-		result = 3
-	}
-
-	return result
 }
